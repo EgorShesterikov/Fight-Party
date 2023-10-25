b714130a8cd4d722a05d4936c8ca4e328086ba70
Egor Shesterikov <sterik.game@mail.ru>
2023-10-25 21:27:49 +0300

    Add play scene

--- Assets/Save/BaseSaveConfig.cs
+++ Assets/Save/BaseSaveConfig.cs
@@ -5,9 +5,9 @@ namespace FightParty.Save
 {
     public abstract class BaseSaveConfig : ScriptableObject
     {
-        protected const string TypeSaveFile = ".json";
+        private const string TypeSaveFile = ".json";
 
-        [SerializeField] protected string _fileName;
+        [SerializeField] private string _fileName;
 
         public string FileName => _fileName + TypeSaveFile;
         public string SavePatch => Path.Combine(Application.persistentDataPath, _fileName);
